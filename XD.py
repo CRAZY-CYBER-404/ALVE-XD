e549465b7822166c7ea8c8f7d310a320fb3c45d9
ALVE RINKU <132809768+CRAZY-CYBER-404@users.noreply.github.com>
2024-01-01 03:30:20 +0600

    Update XD.py

--- XD.py
+++ XD.py
@@ -1,6 +1,6 @@
 import os, platform, time, sys
 os.system('git pull')
 bit = platform.architecture()[0]
-if bit == '64bit':import ALVE64
-elif bit == '32bit':import ALVE32
+if bit == '64bit':import RINKU64
+elif bit == '32bit':import RINKU32
 
