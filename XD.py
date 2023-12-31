af43cebc6e6ced1ecf4fe4df1f7e60e4ccfb519c
ALVE RINKU <132809768+CRAZY-CYBER-404@users.noreply.github.com>
2024-01-01 02:43:46 +0600

    Update XD.py

--- XD.py
+++ XD.py
@@ -1,8 +1,8 @@
-import os, platform, time, sys
-#os.system('pip install requests')
+import os, platform, sys
+from time import sleep as slp
 os.system('git pull --quiet 2>/dev/null')
 bit = platform.architecture()[0]
-if bit == '32bit':print('\3[0;97m[•] \3[1;32mCONGRATULATIONS 64BIT SUCCESS')sleep.time(05)
+if bit == '32bit':print('\3[0;97m[•] \3[1;32mCONGRATULATIONS 64BIT SUCCESS')slp(05)
 
 import ALVE32
 
