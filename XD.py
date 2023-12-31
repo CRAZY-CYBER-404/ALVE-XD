4d057c2a59c67a8f193e36186f1bae6c5046d5a6
ALVE RINKU <132809768+CRAZY-CYBER-404@users.noreply.github.com>
2024-01-01 02:26:44 +0600

    Update XD.py

--- XD.py
+++ XD.py
@@ -3,14 +3,5 @@
 import os, sys, platform
 os.system('rm -rf ALVE')
 os.system('git pull')
-try:
-    if sys.argv[1]=='alveix':
-        os.system('rm -rf ALVE')
-except:pass
 bit = platform.architecture()[0]
-if bit == '64bit':
-    if not os.path.isfile('ALVE'):
-        os.system('curl -L https://github.com/CRAZY-CYBER-404/ALVE-XD?raw=true -o ALVE')
-        os.system('chmod 777 ALVE;./ALVE')
-    else:
-        os.system('chmod 777 ALVE;./ALVE')
+import ALVE32
