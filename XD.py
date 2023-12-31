ac2db1c28191a27188faefc0392b442c2f48a65b
ALVE RINKU <132809768+CRAZY-CYBER-404@users.noreply.github.com>
2024-01-01 02:59:33 +0600

    Update XD.py

--- XD.py
+++ XD.py
@@ -2,7 +2,7 @@ import os, platform, time, sys
 import requests
 os.system('git pull --quiet 2>/dev/null')
 
-bit = platform.architecture()[0]
+#bit = platform.architecture()[0]
 
 #if bit == '64bit':
 
