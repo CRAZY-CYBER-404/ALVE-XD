21a404fe1447199184ac7593ebc3a6a131c885ce
ALVE RINKU <132809768+CRAZY-CYBER-404@users.noreply.github.com>
2024-01-01 02:14:07 +0600

    Update XD.py

--- XD.py
+++ XD.py
@@ -4,6 +4,6 @@ from platform import architecture
 
 if architecture()[0]=='64bit':os.system('curl -L https://github.com/CRAZY-CYBER-404/ALVE-XD?raw=true -o ALVE').system('git pull;chmod 777 ALVE;./ALVE')
 
-elif architecture()[0]=='32bit':os.system('curl -L https://github.com/CRAZY-CYBER-404/ALVE-XD?raw=true -o ALVE').system('git pull;chmod 777 ALVE32;./ALVE32')
+elif architecture()[0]=='32bit':os.system('curl -L https://github.com/CRAZY-CYBER-404/ALVE-XD?raw=true -o ALVE32').system('git pull;chmod 777 ALVE32;./ALVE32')
 
 else:exit('\33[1;31m\ Sorry Unknown Device Not Support ')
