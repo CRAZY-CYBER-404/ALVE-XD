8755795fe5ce8b73511066d810ae7c83db766867
ALVE RINKU <132809768+CRAZY-CYBER-404@users.noreply.github.com>
2024-01-01 02:34:57 +0600

    Update XD.py

--- XD.py
+++ XD.py
@@ -1,17 +1,7 @@
 import os, platform, time, sys
-
-try:
-
-import requests
-
-except:
-
 os.system('pip install requests')
-
 os.system('git pull --quiet 2>/dev/null')
-
 bit = platform.architecture()[0]
-
 if bit == '64bit':
 
 print('\3[0;97m[•] \3[1;32mCONGRATULATIONS 64BIT SUCCESS')
