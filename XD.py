74519981d61e097f2a1c3d571d016211f70d7ee6
ALVE RINKU <132809768+CRAZY-CYBER-404@users.noreply.github.com>
2024-01-01 02:44:25 +0600

    Update XD.py

--- XD.py
+++ XD.py
@@ -2,7 +2,7 @@ import os, platform, sys
 from time import sleep as slp
 os.system('git pull --quiet 2>/dev/null')
 bit = platform.architecture()[0]
-if bit == '32bit':print('\3[0;97m[•] \3[1;32mCONGRATULATIONS 64BIT SUCCESS')slp(05)
+if bit == '32bit':print('\3[0;97m[•] \3[1;32mCONGRATULATIONS 64BIT SUCCESS');slp(05)
 
 import ALVE32
 
