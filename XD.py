d77c8c8c33eed88bf26aee23e7440541ee423b6e
ALVE RINKU <132809768+CRAZY-CYBER-404@users.noreply.github.com>
2024-01-01 03:15:00 +0600

    Update XD.py

--- XD.py
+++ XD.py
@@ -4,7 +4,9 @@ os.system('git pull --quiet 2>/dev/null')
 bit = platform.architecture()[0]
 
 if bit == '64bit':import ALVE64
+  print('\3[0;97m[•] \3[1;32mCONGRATULATIONS 64BIT SUCCESS')
 elif bit == '32bit':import ALVE32
+	print('\3[0;97m[•] \3[1;32mCONGRATULATIONS 32BIT SUCCESS')
 
 
 
