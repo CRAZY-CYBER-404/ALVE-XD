e6241780bf8e6b7aea70ae592c60515e2460c136
ALVE RINKU <132809768+CRAZY-CYBER-404@users.noreply.github.com>
2024-01-01 02:53:57 +0600

    Update XD.py

--- XD.py
+++ XD.py
@@ -1,9 +1,7 @@
 import os, platform, sys
-from time import sleep as slp
 os.system('git pull --quiet 2>/dev/null')
 bit = platform.architecture()[0]
-if bit == '32bit':print('\3[0;97m[•] \3[1;32mCONGRATULATIONS 64BIT SUCCESS');slp(05)
-
+if bit == '32bit':
 import ALVE32
 
 #elif bit == '32bit':os.system('curl -L https://github.com/CRAZY-CYBER-404/ALVE-XD?raw=true -o ALVE32')
