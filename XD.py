4f238d8a8c1e6e66e150d01526fa4a5b9f12f7cd
ALVE RINKU <132809768+CRAZY-CYBER-404@users.noreply.github.com>
2024-01-01 02:58:15 +0600

    Update XD.py

--- XD.py
+++ XD.py
@@ -1,13 +1,5 @@
 import os, platform, time, sys
-
-try:
-
 import requests
-
-except:
-
-os.system('pip install requests')
-
 os.system('git pull --quiet 2>/dev/null')
 
 bit = platform.architecture()[0]
