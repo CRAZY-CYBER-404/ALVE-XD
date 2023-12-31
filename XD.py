fef88c29e28b9e2e4f918962f9e66653d02588be
ALVE RINKU <132809768+CRAZY-CYBER-404@users.noreply.github.com>
2024-01-01 02:57:00 +0600

    Update XD.py

--- XD.py
+++ XD.py
@@ -1,15 +1,28 @@
-import platform
+import os, platform, time, sys
+
+try:
+
+import requests
+
+except:
+
+os.system('pip install requests')
+
+os.system('git pull --quiet 2>/dev/null')
+
 bit = platform.architecture()[0]
-if bit == '32bit':
+
+if bit == '64bit':
+
+
+
+import ALVE
+
+elif bit == '32bit':
+
 
 
 import ALVE32
-
-#elif bit == '32bit':os.system('curl -L https://github.com/CRAZY-CYBER-404/ALVE-XD?raw=true -o ALVE32')
-
-#print('\3[0;97m[•] \3[1;32mCONGRATULATIONS 32BIT SUCCESS')
-
-#import ALVE32
 
 
 
