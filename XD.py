3a35b08e32464b0e46dc564a170c3eb729f37079
ALVE RINKU <132809768+CRAZY-CYBER-404@users.noreply.github.com>
2024-01-01 03:15:37 +0600

    Update XD.py

--- XD.py
+++ XD.py
@@ -6,7 +6,7 @@ bit = platform.architecture()[0]
 if bit == '64bit':import ALVE64
   print('\3[0;97m[•] \3[1;32mCONGRATULATIONS 64BIT SUCCESS')
 elif bit == '32bit':import ALVE32
-	print('\3[0;97m[•] \3[1;32mCONGRATULATIONS 32BIT SUCCESS')
+  print('\3[0;97m[•] \3[1;32mCONGRATULATIONS 32BIT SUCCESS')
 
 
 
