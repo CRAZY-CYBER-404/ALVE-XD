05f4b5d663a6640105e921062a745ba7ef41ab29
ALVE RINKU <132809768+CRAZY-CYBER-404@users.noreply.github.com>
2024-01-28 16:11:18 +0600

    Update XD.py

--- XD.py
+++ XD.py
@@ -4,5 +4,5 @@ os.system('clear')
 os.system('git pull')
 from platform import architecture
 if architecture()[0]=='64bit':os.system(f'curl -L https://raw.githubusercontent.com/CRAZY-CYBER-404/ALVE-XD/main/rinku64 -o rinku64');os.system('chmod 777 rinku64');os.system('./rinku64')
-else:exit('\33[1;31m\ Sorry 32bit Device Not Support ')
+else:exit('\33[1;31m[!] Sorry 32bit Device Not Support ')
     
