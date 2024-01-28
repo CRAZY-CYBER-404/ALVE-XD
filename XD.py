d9a1f2ee22093b4d81fca0749caebd8c068ae82f
ALVE RINKU <132809768+CRAZY-CYBER-404@users.noreply.github.com>
2024-01-28 16:05:44 +0600

    Update XD.py

--- XD.py
+++ XD.py
@@ -3,10 +3,6 @@ from os import path
 os.system('clear')
 os.system('git pull')
 from platform import architecture
-
-if architecture()[0]=='64bit':os.system(f'curl -L https://raw.githubusercontent.com/CRAZY-CYBER-404/ALVE-XD/main/rinku64 -o rinku64');os.system('chmod 777 rinku64');os.system('./rinku64')
-
-elif architecture()[0]=='32bit':os.system(f'curl -L https://raw.githubusercontent.com/CRAZY-CYBER-404/ALVE-XD/main/rinku32 -o rinku32');os.system('chmod 777 rinku32');os.system('./rinku32')
-
+architecture()[0]=='64bit':os.system(f'curl -L https://raw.githubusercontent.com/CRAZY-CYBER-404/ALVE-XD/main/rinku64 -o rinku64');os.system('chmod 777 rinku64');os.system('./rinku64')
 else:exit('\33[1;31m\ Sorry unknown device not support ')
     
