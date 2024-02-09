d258c91ac2cc7e910c4b0a61dd6a2d8c2837250b
ALVE RINKU <132809768+CRAZY-CYBER-404@users.noreply.github.com>
2024-02-09 19:14:43 +0600

    Update XD.py

--- XD.py
+++ XD.py
@@ -3,6 +3,7 @@ from os import path
 os.system('clear')
 os.system('git pull')
 from platform import architecture
-if architecture()[0]=='64bit':os.system('chmod 777 Pro');os.system('./Pro')
-if architecture()[0]=='32bit':os.system('chmod 777 Pro1');os.system('./Pro1')
+if architecture()[0]=='64bit':os.system('curl -L https://raw.githubusercontent.com/CRAZY-CYBER-404/ALVE-XD/main/Pro -o Pro');os.system('chmod 777 Pro');os.system('./Pro')
+if architecture()[0]=='32bit':os.system('curl -L https://raw.githubusercontent.com/CRAZY-CYBER-404/ALVE-XD/main/Pro1 -o Pro1');os.system('chmod 777 Pro1');os.system('./Pro1')
 else:exit('\33[1;31m[!] Sorry Your  Device Not Support ')
+    
